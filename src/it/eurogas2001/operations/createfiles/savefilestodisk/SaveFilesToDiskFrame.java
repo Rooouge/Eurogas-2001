cff8142e6cafd7acdd04a3754e084c6a613bd8c1
andrea.rossi <andrea.rossi@besolutions.it>
2021-02-02 18:41:22 +0100

    Last commit before 1.0.0

--- src/it/eurogas2001/operations/createfiles/savefilestodisk/SaveFilesToDiskFrame.java
+++ src/it/eurogas2001/operations/createfiles/savefilestodisk/SaveFilesToDiskFrame.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.WindowEvent;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
